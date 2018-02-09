7f2f48165f1385ccaed6e94f8625b4e6472808ca
heather <hmbee3@gmail.com>
2018-02-09 15:08:21 -0600

    updated with heroku database2

--- web/Match/database.php
+++ web/Match/database.php
@@ -1,31 +1,28 @@
 <?php
 
    function connect_db() {
-      // default Heroku Postgres configuration URL
       $dbUrl = getenv('DATABASE_URL');
-
       if (empty($dbUrl)) {
- // example localhost configuration URL with postgres username and a database called cs313db
-         $dbUrl = "postgres://postgres:password@localhost:5432/cs313db";
-      }
+         //$dbUrl = "postgres://postgres:33433233@127.0.0.1:5432/match";
+      
 
-      $dbopts = parse_url($dbUrl);
-
+      /*$dbopts = parse_url($dbUrl);
 
       $dbHost = $dbopts["host"];
       $dbPort = $dbopts["port"];
       $dbUser = $dbopts["user"];
       $dbPassword = $dbopts["pass"];
-      $dbName = ltrim($dbopts["path"],'/');
-      
+      $dbName = ltrim($dbopts["path"],'/');*/
+
       try {
       $user = 'heather';
       $password = '33433233';
-      return $db = new PDO("pgsql:host=127.0.0.1;port=5432;dbname=match", $dbUser, $dbPassword);
+      return $db = new PDO("pgsql:host=127.0.0.1;port=5432;dbname=match", $user, $password);
    } catch (PDOException $ex) {
       echo 'Error!: ' . $ex.getMessage();
       die();        
      } 
+  }
    }
 
 ?>
