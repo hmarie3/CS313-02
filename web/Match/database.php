421e2442b8d96db13aa3acb5c06efd295aeb54fb
heather <hmbee3@gmail.com>
2018-02-09 15:22:37 -0600

    updated with heroku database3

--- web/Match/database.php
+++ web/Match/database.php
@@ -3,26 +3,32 @@
    function connect_db() {
       $dbUrl = getenv('DATABASE_URL');
       if (empty($dbUrl)) {
-         //$dbUrl = "postgres://postgres:33433233@127.0.0.1:5432/match";
-      
+         try {
+            $user = 'heather';
+            $password = '33433233';
+            return $db = new PDO("pgsql:host=127.0.0.1;port=5432;dbname=match", $user, $password);
+         } 
+         catch (PDOException $ex) {
+            echo 'Error!: ' . $ex.getMessage();
+            die();        
+         } 
+      }  
+      else {
+         $dbopts = parse_url($dbUrl);
 
-      /*$dbopts = parse_url($dbUrl);
+         $dbHost = $dbopts["host"];
+         $dbPort = $dbopts["port"];
+         $dbUser = $dbopts["user"];
+         $dbPassword = $dbopts["pass"];
+         $dbName = ltrim($dbopts["path"],'/');
 
-      $dbHost = $dbopts["host"];
-      $dbPort = $dbopts["port"];
-      $dbUser = $dbopts["user"];
-      $dbPassword = $dbopts["pass"];
-      $dbName = ltrim($dbopts["path"],'/');*/
-
-      try {
-      $user = 'heather';
-      $password = '33433233';
-      return $db = new PDO("pgsql:host=127.0.0.1;port=5432;dbname=match", $user, $password);
-   } catch (PDOException $ex) {
-      echo 'Error!: ' . $ex.getMessage();
-      die();        
-     } 
-  }
+         try {
+            $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+         }
+         catch (PDOException $ex) {
+         die();
+         }
+      }
    }
 
 ?>
