6130e20c1f5785bf9746014ceca93805744a6a70
heather <hmbee3@gmail.com>
2018-02-11 09:02:53 -0600

    trying to match team05 snytax2

--- web/Match/queries.php
+++ web/Match/queries.php
@@ -2,12 +2,15 @@
 
 function getCategories() {
    $db = connect_db();
+   $statement = $db->prepare("SELECT * FROM scores INNER JOIN match_users ON scores.userid = match_users.userid");
+   $statement->execute();
+   /*
    $sql = 'SELECT * FROM scores INNER JOIN match_users ON scores.userid = match_users.userid';
    $stmt = $db->prepare($sql);
    $stmt->execute();
    $categories = $stmt->fetchAll();
    $stmt->closeCursor();
-   return $categories;
+   return $categories;*/
 }
 
 ?>
