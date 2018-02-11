921b207cbcb3b36add62b9c367e56dbe4d836504
heather <hmbee3@gmail.com>
2018-02-11 08:30:24 -0600

    trying a different query

--- web/Match/queries.php
+++ web/Match/queries.php
@@ -2,7 +2,7 @@
 
 function getCategories() {
    $db = connect_db();
-   $sql = 'SELECT * FROM scores INNER JOIN match_users ON scores.userid = match_users.userid';
+   $sql = 'SELECT * FROM scores, match_users INNER JOIN match_users ON scores.userid = match_users.userid';
    $stmt = $db->prepare($sql);
    $stmt->execute();
    $categories = $stmt->fetchAll();
