d2793f30902231247bc617d925c2639438afff79
heather <hmbee3@gmail.com>
2018-02-13 14:45:20 -0600

    test.php added

--- web/Match/test.php
+++ web/Match/test.php
@@ -1,35 +1,7 @@
 <?php
-
-      $dbUrl = getenv('DATABASE_URL');
-      echo("Printing database URL: $dbUrl");
-      if (empty($dbUrl)) {
-         try {
-            $user = 'heather';
-            $password = '33433233';
-            return $db = new PDO("pgsql:host=127.0.0.1;port=5432;dbname=match", $user, $password);
-         } 
-         catch (PDOException $ex) {
-            echo 'Error!: ' . $ex.getMessage();
-            die();        
-         } 
-      }  
-      else {
-         $dbopts = parse_url($dbUrl);
-         $dbHost = $dbopts["host"];
-         $dbPort = $dbopts["port"];
-         $dbUser = $dbopts["user"];
-         $dbPassword = $dbopts["pass"];
-         $dbName = ltrim($dbopts["path"],'/');
-
-         try {
-          $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
-         }
-         catch (PDOException $ex) {
-          print "<p>error: $ex->getMessage() </p>\n\n";
-          die();
-         }
-      }
+   include_once "database.php";
    
+   $db = connect_db();
    $sql = 'SELECT * FROM scores INNER JOIN match_users ON scores.userid = match_users.userid';
    $stmt = $db->prepare($sql);
    $stmt->execute();
