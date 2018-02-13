4de850cd2911df327eaed049f6783d256aa5bf0d
heather <hmbee3@gmail.com>
2018-02-13 14:47:37 -0600

    test.php added

--- web/Match/database.php
+++ web/Match/database.php
@@ -7,7 +7,7 @@
          try {
             $user = 'heather';
             $password = '33433233';
-            return $db = new PDO("pgsql:host=127.0.0.1;port=5432;dbname=match", $user, $password);
+            $db = new PDO("pgsql:host=127.0.0.1;port=5432;dbname=match", $user, $password);
          } 
          catch (PDOException $ex) {
             echo 'Error!: ' . $ex.getMessage();
@@ -30,6 +30,7 @@
           die();
          }
       }
+      return $db;
    }
 
 ?>
