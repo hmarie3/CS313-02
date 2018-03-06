edd510888ef610e834c4d2186782a86c0a477ca4
heather <hmbee3@gmail.com>
2018-03-06 09:36:37 -0600

    trying to fix files

---
+++ web/homepage.js
@@ -0,0 +1,13 @@
+function grow() {
+  document.getElementById("menu").style.padding = "50px";
+  setTimeout(bold, 100);
+}
+
+function bold() {
+  document.getElementById("link").style.fontWeight = "bold";
+}
+
+function shrink() {
+  document.getElementById("menu").style.padding = "15px";
+  document.getElementById("link").style.fontWeight = "normal";
+}
